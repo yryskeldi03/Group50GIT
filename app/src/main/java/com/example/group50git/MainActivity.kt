c5b1deb55675cd98d58af4ce7de0ecde41b7829e
ymajrambekov@gmail.com <ymajrambekov@gmail.com>
2023-04-20 20:09:26 +0600

    jdkf

--- app/src/main/java/com/example/group50git/MainActivity.kt
+++ app/src/main/java/com/example/group50git/MainActivity.kt
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    //jkdfjd
+    //jkdfjd
+    //jkdfjd
+    //jkdfjd
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         олваов
-        // second branch code
+        // third branch code
+        //kdfjdk
+        //kdfjdk
+        //kdfjdk
+        //kdfjdk
     }
 }
