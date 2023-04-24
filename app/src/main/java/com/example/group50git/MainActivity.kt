ffb95589dafee64e9a143c9f09c0afea97038da0
ymajrambekov@gmail.com <ymajrambekov@gmail.com>
2023-04-24 19:51:18 +0600

    added kotlin-dsl

--- app/src/main/java/com/example/group50git/MainActivity.kt
+++ app/src/main/java/com/example/group50git/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.group50git
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // added kotlin-dsl
     }
 }
