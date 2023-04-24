385c26b40bd15b8a0e57db98b6abc81bf334e18a
ymajrambekov@gmail.com <ymajrambekov@gmail.com>
2023-04-24 20:18:34 +0600

    added app architecture

--- app/src/main/java/com/example/group50git/MainActivity.kt
+++ app/src/main/java/com/example/group50git/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // added kotlin-dsl
+        // added app architecture
     }
 }
